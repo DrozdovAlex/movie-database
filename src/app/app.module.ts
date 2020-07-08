3d898b5ba7681c7544730ac24d4812f436ae4718
DrozdovAlex <vip.alexd@gmail.com>
2020-07-08 20:32:10 +0700

    create info, list, search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule, MatInputModule} from '@angular/material';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieSearchComponent,
+    MovieListComponent,
+    MovieInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
